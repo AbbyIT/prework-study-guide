3865b8416849a77a176ede70fc9e931a23e6e831
Abby E <saegbo@yahoo.com>
2022-11-13 18:45:31 +0000

    Adding More Array of functions to code

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,38 @@
 
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
+function listtopics() {
+    
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') { 
+function selectTopic () {
+
+if (randomTopic === 'HTML') { 
 
     console.log("Lets study HTML!");
-}   else if (topic === 'CSS') {
+}   else if (randomTopic === 'CSS') {
 
     console.log("Lets study CSS!")
 
-}    else if (topic === 'Git') {
+}    else if (randomTopic === 'Git') {
 
     console.log("Lets study Git!")
 
-}    else if (topic ==='JavaScript') { 
+}    else if (randomTopic ==='JavaScript') { 
      console.log ("Lets study JavaScript!")
 
 }else { 
      console.log('Please try again!');
+
+    }
 }
 
-*/
+console.log('Here are the topics we learned through Prework:');
+
+listtopics()
+selectTopic()
+
